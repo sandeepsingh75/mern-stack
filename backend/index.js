50277ae211b4cae47ad3da6a20f51495694fba79
Sandeep Singh <sandeepsinghrana101@gmail.com>
2023-10-23 15:24:52 +0530

    create model in node js

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const lib = require("./lib.js");
+// import {sum,diff} from "./lib.js";
+const fs = require("fs");
+
+// const file = fs.readFileSync("./demo.txt","utf-8");
+const t1 = performance.now();
+const file = fs.readFile("./demo.txt","utf-8", (err,txt)=>{
+    console.log(txt)
+});
+
+// console.log(file);
+const t2 = performance.now();
+console.log(lib.sum(1,6),lib.diff(5,2));
+// console.log(sum(1,8),diff(5,3));
+
+
+console.log(t2-t1)
