02b35542f84afd1a114c0d3f3ff0019bbc6df978
sandeep singh <sandeepsinghrana100@gmail.com>
2023-11-29 18:26:29 +0530

    add comments for different data type

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,21 @@
-const express =require("express");
+const http = require("http");
+const fs = require('fs');
 
-const server = express();
+const index = fs.readFileSync('index.html','utf-8');
+const data = fs.readFileSync('data.json','utf-8');
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  console.log("server started");
+  res.setHeader("DummyHeader", "DummyValue");    
+  // res.setHeader("Content-Type", "text/html");               // set "Content-Type", "text/html" if we send html content
+  res.setHeader("Content-Type", "application/json"); //set "Content-Type","application/json" if we send json data or file
+
+
+
+res.end(data);
+  res.end(index);
+  // res.end(JSON.stringify(data));        
+  // res.end('<h1>Hello</h1>')
+});
 
 server.listen(8080);
