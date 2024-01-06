978aedfda65c1f74109a256def54ef1cdf8de781
sandeep singh <sandeepsinghrana100@gmail.com>
2024-01-06 17:38:23 +0530

    send json data in response with header

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,25 @@
 const http = require("http");
 const fs = require('fs');
 
-const index = fs.readFileSync('index.html','utf-8');
-const data = fs.readFileSync('data.json','utf-8');
+const index = fs.readFileSync('index.html','utf-8')
+const data1 = fs.readFileSync('data.json','utf-8')
+
+const data = { age: 5 };
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  console.log("server started");
-  res.setHeader("DummyHeader", "DummyValue");    
-  // res.setHeader("Content-Type", "text/html");               // set "Content-Type", "text/html" if we send html content
-  res.setHeader("Content-Type", "application/json"); //set "Content-Type","application/json" if we send json data or file
+  console.log("server created");
+  res.setHeader("dummy", "DummyValue");
+  // res.setHeader('Content-Type','application/json')
+  // res.end(JSON.stringify(data));
+
+  //  ---------------- Send Text data in Response --------------------
+  // res.setHeader("Content-Type", "text/html");
+  // res.end("hello");
+  // res.end(index);
 
 
-
-res.end(data);
-  res.end(index);
-  // res.end(JSON.stringify(data));        
-  // res.end('<h1>Hello</h1>')
+  // --------------- send JSON data in response -----------------
+  res.setHeader('Content-Type','application/json')
+  res.end(data1)
 });
 
 server.listen(8080);
